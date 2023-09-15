cdb432b7974d620d738e7eaa225a550d5ea84e4b
user-faiz <email2faizm@gmail.com>
2023-09-16 02:28:18 +0530

    combining all the reducers in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { combineReducers } from 'redux';
+import counterReducer from './counterSlice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default rootReducer;
