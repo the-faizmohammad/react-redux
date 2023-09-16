3ea640ed3e3e72dfd73cd438b4430f8d6fef1661
user-faiz <email2faizm@gmail.com>
2023-09-16 09:40:35 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; // Import the Provider component
+import store from './redux/store'; // Import your Redux store
+import { BrowserRouter as Router } from 'react-router-dom'; // Import the Router component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
